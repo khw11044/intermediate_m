29de28311bdf8b4b8e86b70fa004915147020d60
khw11044@gmail.com <khw11044@naver.com>
2024-06-11 11:53:01 +0900

    upload

--- multimodal.py
+++ multimodal.py
@@ -7,7 +7,6 @@ import os
 import time
 from langchain import hub
 from langchain_core.prompts import PromptTemplate
-from pathlib import Path
 
 # !pip install openai-whisper
 import whisper
@@ -38,14 +37,6 @@ def summarize_text(text):
     summary = chain.invoke({"transcript": docs})
     return summary.content
 
-def get_download_folder():
-    """사용자의 다운로드 폴더 경로를 반환합니다."""
-    if os.name == 'nt':
-        from pathlib import Path
-        download_folder = str(Path.home() / "Downloads")
-    else:
-        download_folder = str(Path.home() / "Downloads")
-    return download_folder
 
 # Streamlit app
 st.title("유튜브 뉴스 영상 SEO 컨텐츠 기사로 만들기")
@@ -54,15 +45,29 @@ st.write("음성 파일을 업로드하면 기사글을 작성해줍니다.")
 # pip install pytube
 from pytube import YouTube
 from pathlib import Path
+
+st.markdown(f"<a href='https://www.youtube.com/watch?v=4EzXnCfB5oU' style='font-size:14px;'>예시: https://www.youtube.com/watch?v=4EzXnCfB5oU</a>", unsafe_allow_html=True)
 address = st.text_input('유튜브 주소를 입력하고 엔터를 눌러주세요.')
 if address:
-    st.markdown(f"<a href='{address}' style='font-size:14px;'>오디오 파일 다운로드중... : {address}</a>", unsafe_allow_html=True)
-    download_path = get_download_folder()
-    yt = YouTube(address)
-    audio_stream = yt.streams.filter(only_audio=True).first()
-    file_path = audio_stream.download(download_path)
-    file_name = os.path.basename(file_path)
-    st.markdown(f"<p style='font-size:14px;'>다운로드 위치: {file_name}</p>", unsafe_allow_html=True)
+    try:
+        # 임시 디렉토리에 파일 다운로드
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            yt = YouTube(address)
+            audio_stream = yt.streams.filter(only_audio=True).first()
+            file_path = audio_stream.download(output_path=tmpdirname)
+            file_name = os.path.basename(file_path)
+            
+            st.success("오디오 파일이 다운로드되었습니다.")
+            # 파일을 다운로드할 수 있는 링크 제공
+            with open(file_path, "rb") as file:
+                btn = st.download_button(
+                    label="오디오 파일 다운로드",
+                    data=file,
+                    file_name=file_name,
+                    mime="audio/mp4"
+                )
+    except Exception as e:
+        st.error(f"오디오 파일을 다운로드하는 중 오류가 발생했습니다: {e}")
 
 
 
@@ -73,6 +78,7 @@ if audio_file is not None:
         temp_file.write(audio_file.read())
         temp_file_path = temp_file.name
 
+    print('temp_file_path:',temp_file_path)
     st.audio(temp_file_path, format="audio/wav")
     transcription = transcribe_audio(temp_file_path)
     st.subheader("Transcription")
